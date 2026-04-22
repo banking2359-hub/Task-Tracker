a22a4ac2ee710ee318f800c1a86cc29a31046da7
Ban <banking2359@gmail.com>
2026-04-22 11:18:05 +0300

    adding lottie animation on the onbeard swiper

--- src/component/onBoarding.js
+++ src/component/onBoarding.js
@@ -1,27 +1,44 @@
 import Onboarding from "react-native-onboarding-swiper";
+import LottieView from "lottie-react-native";
 import { Image } from "react-native";
 
+
 export default function OnBoardingScreen() {
+
     return (
         <Onboarding
             pages={[
                 {
                     backgroundColor: "#5e70b5",
-                    Image: <Image source={require('../../assets/favicon.png')} />,
-                    title: "Focus Time",
-                    subtitle: "How much time you can stay focus"
+                    image: <LottieView
+                        autoPlay
+                        loop
+                        style={{ width: 400, height: 400 }}
+                        source={require('../../assets/animation/setPlan.json')}
+                    />,
+                    title: "ADD ACTIVITY",
+                    subtitle: "add activity you want to focus on"
                 },
                 {
                     backgroundColor: '#d36f86',
-                    Image: <Image source={require('../../assets/favicon.png')} />,
-                    title: "Focus Time",
-                    subtitle: "How much time you can stay focus"
+                    image: <LottieView
+                        autoPlay
+                        loop
+                        source={require('../../assets/animation/study.json')}
+                        style={{width:450,height:450}}
+                    />,
+                    title: "FOCUS TIME",
+                    subtitle: "select a time you plane for your added task and start to achieve  it . "
                 },
                 {
-                    backgroundColor: '#8adf22',
-                    Image: <Image source={require('../../assets/favicon.png')} />,
-                    title: "Focus Time",
-                    subtitle: "How much time you can stay focus"
+                    backgroundColor: '#6dc16f',
+                    image: <LottieView
+                        autoPlay
+                        source={require('../../assets/animation/checkList.json')}
+                        style={{width:500,height:500}}
+                    />,
+                    title: "RECENT TASK",
+                    subtitle: "see what task you seccesfully acomplish as your plane"
                 },
 
             ]}
